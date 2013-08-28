b89376b89b548d8ca3a1d8495e2c148ee5a538f5
Peter Rabbitson <ribasushi@cpan.org>
2013-08-28 14:01:00 +0200

    Add 5.8.1 just because

--- maint/travis-ci_scripts/30_before_script.bash
+++ maint/travis-ci_scripts/30_before_script.bash
@@ -79,7 +79,7 @@ else
   parallel_installdeps_notest Test::Exception Encode::Locale Test::Fatal
   parallel_installdeps_notest Test::Warn bareword::filehandles B::Hooks::EndOfScope Test::Differences HTTP::Status
   parallel_installdeps_notest Test::Pod::Coverage Test::EOL Devel::GlobalDestruction Sub::Name MRO::Compat Class::XSAccessor URI::Escape HTML::Entities
-  parallel_installdeps_notest YAML LWP Class::Trigger JSON::XS DBI DateTime::Format::Builder Class::Accessor::Grouped Package::Variant
+  parallel_installdeps_notest YAML LWP Class::Trigger DBI DateTime::Format::Builder Class::Accessor::Grouped Package::Variant
   parallel_installdeps_notest Moose Module::Install JSON SQL::Translator
 
   if [[ -n "DBICTEST_FIREBIRD_DSN" ]] ; then
